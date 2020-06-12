4e06e719e8d17a03d04c369209d625e719ea759d
Antoine Kaufmann <antoinek@mpi-sws.org>
2020-06-12 21:26:00 +0200

    factor out netsim_common library

--- net_tap/net_tap.c
+++ net_tap/net_tap.c
@@ -28,108 +28,14 @@
 #include <string.h>
 #include <sys/ioctl.h>
 #include <sys/mman.h>
-#include <sys/un.h>
-#include <sys/socket.h>
-#include <sys/stat.h>
 #include <unistd.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
 
-#include <cosim_eth_proto.h>
+#include <netsim.h>
 
-static uint8_t *d2n_queue;
-static size_t d2n_pos;
-static size_t d2n_elen;
-static size_t d2n_enum;
-
-static uint8_t *n2d_queue;
-static size_t n2d_pos;
-static size_t n2d_elen;
-static size_t n2d_enum;
-
+static struct netsim_interface nsif;
 static int tap_fd;
-
-int uxsocket_connect(const char *path)
-{
-    int fd;
-    struct sockaddr_un saun;
-
-    /* prepare and connect socket */
-    memset(&saun, 0, sizeof(saun));
-    saun.sun_family = AF_UNIX;
-    strcpy(saun.sun_path, path);
-
-    if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-        perror("socket failed");
-        return -1;
-    }
-
-    if (connect(fd, (struct sockaddr *) &saun, sizeof(saun)) != 0) {
-        perror("connect failed");
-        return -1;
-    }
-
-    return fd;
-}
-
-int uxsocket_recv(int fd, void *data, size_t len, int *pfd)
-{
-    int *ppfd;
-    ssize_t ret;
-    struct cmsghdr *cmsg;
-    union {
-        char buf[CMSG_SPACE(sizeof(int))];
-        struct cmsghdr align;
-    } u;
-    struct iovec iov = {
-        .iov_base = data,
-        .iov_len = len,
-    };
-    struct msghdr msg = {
-        .msg_name = NULL,
-        .msg_namelen = 0,
-        .msg_iov = &iov,
-        .msg_iovlen = 1,
-        .msg_control = u.buf,
-        .msg_controllen = sizeof(u.buf),
-        .msg_flags = 0,
-    };
-
-    if ((ret = recvmsg(fd, &msg, 0)) != (ssize_t) len) {
-        perror("recvmsg failed");
-        return -1;
-    }
-
-    cmsg = CMSG_FIRSTHDR(&msg);
-    ppfd = (int *) CMSG_DATA(cmsg);
-    if (msg.msg_controllen <= 0 || cmsg->cmsg_len != CMSG_LEN(sizeof(int))) {
-        fprintf(stderr, "accessing ancillary data failed\n");
-        return -1;
-    }
-
-    *pfd = *ppfd;
-    return 0;
-}
-
-void *shm_map(int shm_fd)
-{
-    void *p;
-    struct stat statbuf;
-
-    if (fstat(shm_fd, &statbuf) != 0) {
-        perror("shm_map: fstat failed");
-        return NULL;
-    }
-
-    p = mmap(NULL, statbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd,
-            0);
-    if (p == MAP_FAILED) {
-        perror("shm_map: mmap failed");
-        return NULL;
-    }
-
-    return p;
-}
 
 static int tap_open(const char *name)
 {
@@ -165,14 +71,11 @@ static void d2n_send(volatile struct cosim_eth_proto_d2n_send *s)
 
 static void poll_d2n(void)
 {
+    volatile union cosim_eth_proto_d2n *msg = netsim_d2n_poll(&nsif);
     uint8_t type;
-    volatile union cosim_eth_proto_d2n *msg =
-        (volatile union cosim_eth_proto_d2n *)
-        (d2n_queue + d2n_pos * d2n_elen);
 
     /* message not ready */
-    if ((msg->dummy.own_type & COSIM_ETH_PROTO_D2N_OWN_MASK) !=
-            COSIM_ETH_PROTO_D2N_OWN_NET)
+    if (msg == NULL)
         return;
 
     type = msg->dummy.own_type & COSIM_ETH_PROTO_D2N_MSG_MASK;
@@ -185,9 +88,7 @@ static void poll_d2n(void)
             fprintf(stderr, "poll_d2n: unsupported type=%u\n", type);
     }
 
-    msg->dummy.own_type = (msg->dummy.own_type & COSIM_ETH_PROTO_D2N_MSG_MASK)
-        | COSIM_ETH_PROTO_D2N_OWN_DEV;
-    d2n_pos = (d2n_pos + 1) % d2n_enum;
+    netsim_d2n_done(&nsif, msg);
 }
 
 static void *rx_handler(void *arg)
@@ -197,18 +98,14 @@ static void *rx_handler(void *arg)
     ssize_t len;
 
     while (1) {
-        msg = (volatile union cosim_eth_proto_n2d *)
-                (n2d_queue + n2d_pos * n2d_elen);
-
-        if ((msg->dummy.own_type & COSIM_ETH_PROTO_N2D_OWN_MASK) !=
-                COSIM_ETH_PROTO_N2D_OWN_NET)
-        {
+        msg = netsim_n2d_alloc(&nsif);
+        if (msg == NULL) {
             fprintf(stderr, "coudl not allocate message for rx\n");
             abort();
         }
         rx = &msg->recv;
 
-        len = read(tap_fd, (void *) rx->data, n2d_elen - sizeof(*msg));
+        len = read(tap_fd, (void *) rx->data, nsif.n2d_elen - sizeof(*msg));
         if (len <= 0) {
             perror("rx handler: read failed");
         }
@@ -219,16 +116,12 @@ static void *rx_handler(void *arg)
         // WMB();
         rx->own_type = COSIM_ETH_PROTO_N2D_MSG_RECV |
             COSIM_ETH_PROTO_N2D_OWN_DEV;
-        n2d_pos = (n2d_pos + 1) % n2d_enum;
     }
 }
 
 int main(int argc, char *argv[])
 {
-    struct cosim_eth_proto_dev_intro di;
-    struct cosim_eth_proto_net_intro ni;
-    int cfd, shm_fd;
-    void *p;
+    int sync;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: net_tap TAP_DEVICE_NAME\n");
@@ -239,32 +132,11 @@ int main(int argc, char *argv[])
         return -1;
     }
 
-    if ((cfd = uxsocket_connect("/tmp/cosim-eth")) < 0) {
+    sync = 0;
+    if (netsim_init(&nsif, "/tmp/cosim-eth", &sync) != 0) {
+        close(tap_fd);
         return -1;
     }
-
-    memset(&ni, 0, sizeof(ni));
-    if (send(cfd, &ni, sizeof(ni), 0) != sizeof(ni)) {
-        perror("sending net intro failed");
-        return -1;
-    }
-
-    if (uxsocket_recv(cfd, &di, sizeof(di), &shm_fd)) {
-        return -1;
-    }
-
-    if ((p = shm_map(shm_fd)) == NULL) {
-        return -1;
-    }
-    close(shm_fd);
-
-    d2n_queue = (uint8_t *) p + di.d2n_offset;
-    n2d_queue = (uint8_t *) p + di.n2d_offset;
-    d2n_elen = di.d2n_elen;
-    n2d_elen = di.n2d_elen;
-    d2n_enum = di.d2n_nentries;
-    n2d_enum = di.n2d_nentries;
-    d2n_pos = n2d_pos = 0;
 
     pthread_t worker;
     if (pthread_create(&worker, NULL, rx_handler, NULL) != 0) {
