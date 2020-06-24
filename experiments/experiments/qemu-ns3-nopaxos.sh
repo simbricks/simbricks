f30132ef2332aa6320ef76bbbf64bf6569aa1718
Jialin Li <lijl@comp.nus.edu.sg>
2020-06-24 11:57:53 +0800

    experiments: nopaxos: add sleep to ensure correct execution order of replicas and clients

--- experiments/experiments/qemu-ns3-nopaxos.sh
+++ experiments/experiments/qemu-ns3-nopaxos.sh
@@ -10,8 +10,10 @@ run_corundum_bm r2
 sleep 0.5
 run_ns3_sequencer nopaxos "c0" "r0 r1 r2"
 run_qemu r0 r0 build/qemu-nopaxos-replica-0.tar
+sleep 1
 run_qemu r1 r1 build/qemu-nopaxos-replica-1.tar
 run_qemu r2 r2 build/qemu-nopaxos-replica-2.tar
+sleep 1
 run_qemu c0 c0 build/qemu-nopaxos-client.tar
 client_pid=$!
 wait $client_pid
--- experiments/guests/qemu-nopaxos-client/run.sh
+++ experiments/guests/qemu-nopaxos-client/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 insmod mqnic.ko
 ip link set dev eth0 up
-ip addr add 10.1.0.100/24 broadcast 10.1.0.255 dev eth0
-/root/nopaxos/bench/client -c /root/nopaxos.config -m nopaxos -n 1000
+ip addr add 10.1.0.100/24 dev eth0
+/root/nopaxos/bench/client -c /root/nopaxos.config -m nopaxos -n 2000
 poweroff -f
