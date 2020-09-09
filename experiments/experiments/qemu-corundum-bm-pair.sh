9e54761a5aab1b7d5368e11b850e0d01d75f3070
Hejing Li <hejingli@brain21.mpi-sws.org>
2020-09-10 01:29:53 +0200

    increase sleeping time after run_corundum

--- experiments/experiments/qemu-corundum-bm-pair.sh
+++ experiments/experiments/qemu-corundum-bm-pair.sh
@@ -5,7 +5,7 @@ source common-functions.sh
 init_out qemu-corundum-bm-pair $1
 run_corundum_bm a
 run_corundum_bm b
-sleep 0.5
+sleep 2
 run_wire ab a b
 run_qemu a a build/qemu-pair-server.tar
 run_qemu b b build/qemu-pair-client.tar
