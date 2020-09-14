d9ae8200be9f14669828c8a94af2a2cef7d1e36e
Antoine Kaufmann <antoinek@mpi-sws.org>
2020-09-14 10:42:25 +0200

    experiments: consistently use m5 exit in gem5 guests

--- experiments/guests/gem5-vr-replica-0-cp/run.sh
+++ experiments/guests/gem5-vr-replica-0-cp/run.sh
@@ -4,4 +4,4 @@ insmod mqnic.ko
 ip link set dev eth0 up
 ip addr add 10.1.0.1/24 dev eth0
 /root/nopaxos/bench/replica -c /root/nopaxos.config -i 0 -m vr
-poweroff -f
+/sbin/m5 exit
--- experiments/guests/gem5-vr-replica-2-cp/run.sh
+++ experiments/guests/gem5-vr-replica-2-cp/run.sh
@@ -5,4 +5,4 @@ ip link set dev eth0 up
 ip addr add 10.1.0.3/24 dev eth0
 sleep 2
 /root/nopaxos/bench/replica -c /root/nopaxos.config -i 2 -m vr
-poweroff
+/sbin/m5 exit
