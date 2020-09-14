c63197c06846b1987e32803a89eb01c89281247b
Antoine Kaufmann <antoinek@mpi-sws.org>
2020-09-14 23:40:39 +0200

    experiments: update iperf parameters for gem5-i40e-pair

--- experiments/guests/gem5-pair-i40e-client/run.sh
+++ experiments/guests/gem5-pair-i40e-client/run.sh
@@ -5,5 +5,5 @@ ip link set dev eth0 up
 ip addr add 192.168.64.2/24 dev eth0
 ethtool -K eth0 tso off
 sleep 2
-iperf -l 1M -w 1M -c 192.168.64.1
+iperf -l 1M -w 1M -c 192.168.64.1 -i 1 -P 4
 m5 exit
--- experiments/guests/gem5-pair-i40e-server/run.sh
+++ experiments/guests/gem5-pair-i40e-server/run.sh
@@ -4,5 +4,5 @@ modprobe i40e
 ip link set dev eth0 up
 ip addr add 192.168.64.1/24 dev eth0
 ethtool -K eth0 tso off
-iperf -s -l 1M -w 1M
+iperf -s -l 1M -w 1M -P 4
 m5 exit
