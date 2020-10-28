02c660d6270dbf437b885710aa6fadeb5f34cca1
Jialin Li <lijl@comp.nus.edu.sg>
2020-10-28 12:13:29 +0800

    multiple servers in echo experiment

--- experiments/guests/qemu-echo-client/run.sh
+++ experiments/guests/qemu-echo-client/run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 insmod mqnic.ko
 ip link set dev eth0 up
-ip addr add 10.100.0.2/24 dev eth0
+ip addr add 10.100.0.1/24 dev eth0
 sleep 2
-ping -c 5 10.100.0.1
+ping -c 5 10.100.0.2
+ping -c 5 10.100.0.3
+ping -c 5 10.100.0.4
 poweroff -f
