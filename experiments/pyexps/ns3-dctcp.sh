7410c32e7479ea199828e9f9169d74c2b08d5a9c
Hejing Li <hajeongee@gmail.com>
2020-11-29 07:46:20 +0100

    ns3 dctcp script add latency parameter

--- experiments/pyexps/ns3-dctcp.sh
+++ experiments/pyexps/ns3-dctcp.sh
@@ -13,11 +13,15 @@ OUTDIR_BASE="$EHSIM_BASE/experiments/pyexps"
 
 cd $NS3_BASE
 k_start=0
+k_end=199680
+k_step=2080
 #k_end=199680
-k_end=0
-k_step=8320
+#k_step=8320
 #mtus="1500 4000 9000"
 mtus="1500"
+# link latency corresponds to RTT latency 1us 10us 100us
+#latencies="167ns 1670ns 16us 33us"
+latencies="33us"
 cores=$1
 
 echo $cores
@@ -26,27 +30,34 @@ proc=0
 pids=""
 
 #for k in $(seq $k_start $k_step $k_end)
-for m in $mtus
+for lat in $latencies
 do
-    #echo $k
-    #for m in 1500 4000 9000 # MTU size
-    for k in $(seq $k_start $k_step $k_end)
+    for m in $mtus
     do
-        echo "MtU: $m  K: $k  "
-        ./cosim-dctcp-run.sh $k $m 
-        pid=$!
-        pids="$pids $pid"
-        proc=$(($proc + 1))
-        
-        if [ $proc -eq $cores ]; then
-            for p in $pids; do
-                wait $p
-            done
-            proc=0
-            pids=""
+        #echo $k
+        #for m in 1500 4000 9000 # MTU size
+        for k in $(seq $k_start $k_step $k_end)
+        do
+            echo "latency: $lat MtU: $m  K: $k  "
+            ./cosim-dctcp-run.sh $k $m $lat &
+            pid=$!
+            pids="$pids $pid"
+            proc=$(($proc + 1))
+            
+            if [ $proc -eq $cores ]; then
+                for p in $pids; do
+                    wait $p
+                done
+                proc=0
+                pids=""
 
-        fi
+            fi
+        done
     done
+done
+
+for p in $pids; do
+    wait $p
 done
 
 
