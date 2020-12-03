5f2549b6ebf5e47b513d399f22e6e579443b9680
Hejing Li <hajeongee@gmail.com>
2020-12-03 18:05:52 +0100

    nopaxos.py add intel nic and change naming

--- experiments/pyexps/nopaxos.py
+++ experiments/pyexps/nopaxos.py
@@ -2,43 +2,71 @@ import modes.experiments as exp
 import modes.simulators as sim
 import modes.nodeconfig as node
 
-host_configs = ['bm', 'cycle']
+host_configs = ['qemu', 'gt']
 seq_configs = ['swseq', 'ehseq']
+nic_configs = ['ib', 'cb', 'cv']
 experiments = []
+
+link_rate_opt = '--LinkRate=100Gb/s ' # don't forget space at the end
+link_latency_opt = '--LinkLatency=500ns '
 
 for host_config in host_configs:
     for seq_config in seq_configs:
-        e = exp.Experiment('nopaxos-' + host_config + '-' + seq_config)
-        net = sim.NS3SequencerNet()
-        e.add_network(net)
+        for nic_config in nic_configs:
+            e = exp.Experiment('nopaxos-' + host_config + '-' + nic_config + '-' + seq_config)
+            net = sim.NS3SequencerNet()
+            net.opt = link_rate_opt + link_latency_opt
+            e.add_network(net)
 
-        if host_config == 'bm':
-            host_class = sim.QemuHost
-            nic_class = sim.CorundumBMNIC
-            nc_class = node.CorundumLinuxNode
-        elif host_config == 'cycle':
-            host_class = sim.Gem5Host
-            nic_class = sim.CorundumVerilatorNIC
-            nc_class = node.CorundumLinuxNode
-        else:
-            raise NameError(host_config)
-        nc_class.disk_image = 'nopaxos'
+            # host
+            if host_config == 'qemu':
+                host_class = sim.QemuHost
+                # nic
+                if nic_config == 'ib':
+                    nic_class = sim.I40eNIC
+                    nc_class = node.I40eLinuxNode
+                elif nic_config == 'cb':
+                    nic_class = sim.CorundumBMNIC
+                    nc_class = node.CorundumLinuxNode
+                else:
+                    continue
+                
+            elif host_config == 'gt':
+                host_class = sim.Gem5Host
+                e.checkpoint = False
+                # nic
+                if nic_config == 'ib':
+                    nic_class = sim.I40eNIC
+                    nc_class = node.I40eLinuxNode
+                elif nic_config == 'cb':
+                    nic_class = sim.CorundumBMNIC
+                    nc_class = node.CorundumLinuxNode
+                elif nic_config == 'cv':
+                    nic_class = sim.CorundumVerilatorNIC
+                    nc_class = node.CorundumLinuxNode
+                else:
+                    raise NameError(nic_config)
+            else:
+                raise NameError(host_config)
 
-        if seq_config == 'ehseq':
-            sequencer = sim.create_basic_hosts(e, 1, 'sequencer', net, nic_class,
-                    host_class, nc_class, node.NOPaxosSequencer, ip_start = 100)
-            sequencer[0].sleep = 1
 
-        replicas = sim.create_basic_hosts(e, 3, 'replica', net, nic_class,
-                host_class, nc_class, node.NOPaxosReplica)
-        for i in range(len(replicas)):
-            replicas[i].node_config.app.index = i
-            replicas[i].sleep = 1
+            nc_class.disk_image = 'nopaxos'
 
-        clients = sim.create_basic_hosts(e, 1, 'client', net, nic_class,
-                host_class, nc_class, node.NOPaxosClient, ip_start = 4)
-        for c in clients:
-            c.node_config.app.server_ips = ['10.0.0.1', '10.0.0.2', '10.0.0.3']
-            c.wait = True
+            if seq_config == 'ehseq':
+                sequencer = sim.create_basic_hosts(e, 1, 'sequencer', net, nic_class,
+                        host_class, nc_class, node.NOPaxosSequencer, ip_start = 100)
+                sequencer[0].sleep = 1
 
-        experiments.append(e)
+            replicas = sim.create_basic_hosts(e, 3, 'replica', net, nic_class,
+                    host_class, nc_class, node.NOPaxosReplica)
+            for i in range(len(replicas)):
+                replicas[i].node_config.app.index = i
+                replicas[i].sleep = 1
+
+            clients = sim.create_basic_hosts(e, 1, 'client', net, nic_class,
+                    host_class, nc_class, node.NOPaxosClient, ip_start = 4)
+            for c in clients:
+                c.node_config.app.server_ips = ['10.0.0.1', '10.0.0.2', '10.0.0.3']
+                c.wait = True
+
+            experiments.append(e)
