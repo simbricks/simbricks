13bcc4a14558abe9879c6273ad9016a243a7b2e8
Hejing Li <hajeongee@gmail.com>
2020-12-07 20:22:48 +0100

    result: add udp_scale data parser

---
+++ results/udp_scale.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import pathlib
+import shutil
+import json
+
+if len(sys.argv) != 2:
+    print('Usage: udp_scale.py OUTDIR')
+    sys.exit(1)
+
+basedir = sys.argv[1] + '/'
+types_of_client = [1, 3, 7, 15, 31]
+bw = 1000
+
+for cl in types_of_client:
+    log_path = '%sgt-ib-switch-UDPmicro-%d-%d-1.json' % (basedir, bw, cl)
+
+    try:
+        log = open(log_path, 'r')
+    except:
+        print(f'no file: {log_path}')
+        continue
+    else:
+        exp_log = json.load(log)
+        start_time = exp_log["start_time"]
+        end_time = exp_log["end_time"]
+        diff_time = (end_time - start_time)/60 #min
+        
+        print(diff_time)
+
+        log.close()
+
