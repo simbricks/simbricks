69dbb88693c7a2dd13bad5358a74ba8f8d36276b
Antoine Kaufmann <antoinek@mpi-sws.org>
2020-12-08 19:18:14 +0100

    results: copy over 1g point from udp scale to udp rate experiment

--- results/udp.py
+++ results/udp.py
@@ -9,17 +9,24 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 basedir = sys.argv[1] + '/'
-types_of_bw = [0, 20, 40, 60, 80, 100, 120, 140]
+
+# FIXME: dropped 140 because it looks off
+types_of_bw = [0, 20, 40, 60, 80, 100, 120, 200, 500, 1000]
 
 
 for bw in types_of_bw:
     log_path = '%sgt-ib-wire-UDPs-%dm-1.json' % (basedir, bw)
-    log = open(log_path, 'r')
-    exp_log = json.load(log)
-    start_time = exp_log["start_time"]
-    end_time = exp_log["end_time"]
-    diff_time = (end_time - start_time)/60 #min
-    print('%d\t%f' % (bw, diff_time))
+    if os.path.exists(log_path):
+        log = open(log_path, 'r')
+        exp_log = json.load(log)
+        start_time = exp_log["start_time"]
+        end_time = exp_log["end_time"]
+        diff_time = (end_time - start_time)/60 #min
+        diff_time = str(diff_time)
+    else:
+        diff_time = ''
+
+    print('%d\t%s' % (bw, diff_time))
 
     log.close()
 
