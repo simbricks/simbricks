3fa7661e9fe4171aa5f30e610652bb2036bb3e9e
Jialin Li <lijl@comp.nus.edu.sg>
2021-04-30 17:02:06 +0800

    experiments: add pcap support to net_wire

--- experiments/simbricks/simulators.py
+++ experiments/simbricks/simulators.py
@@ -239,10 +239,13 @@ class I40eNIC(NICSim):
 class WireNet(NetSim):
     def run_cmd(self, env):
         assert len(self.nics) == 2
-        return '%s/sims/net/wire/net_wire %s %s %d %d %d' % \
+        cmd = '%s/sims/net/wire/net_wire %s %s %d %d %d' % \
                 (env.repodir, env.nic_eth_path(self.nics[0]),
                         env.nic_eth_path(self.nics[1]),
                         self.sync_mode, self.sync_period, self.eth_latency)
+        if len(env.pcap_file) > 0:
+            cmd += ' ' + env.pcap_file
+        return cmd
 
 class SwitchNet(NetSim):
     def run_cmd(self, env):
