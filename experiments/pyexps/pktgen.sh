47dbf23833942e8b4eceea65a66467f6e5acc7ca
Hejing Li <hajeongee@gmail.com>
2021-09-15 00:02:49 +0200

    pktgen script dumbbell

--- experiments/pyexps/pktgen.sh
+++ experiments/pyexps/pktgen.sh
@@ -5,6 +5,7 @@ RUN_DIR="/tmp/hejing-work/pktgen"
 NUM_HOST=$1
 ALL_PIDS=""
 PKTGEN_PIDS=""
+SWITCH_PIDS=""
 # -inst num
 run_pktgen(){
     echo "starting host $1"
@@ -32,6 +33,48 @@ run_switch(){
 
     pid=$!
     ALL_PIDS="$ALL_PIDS $pid"
+    return $pid
+}
+
+
+run_switch_dumbbell(){
+    echo "Starting switch dumbbell"
+    SWITCH_EXE=/DS/endhost-networking/work/sim/hejing/simbricks/sims/net/switch/net_switch
+    args_0=""
+    args_1=""
+    iface=0
+    half=$(($1/2))
+    while [ $iface -lt $half ]
+    do
+        args_0="$args_0 -s $RUN_DIR/eth.$iface"
+        #((iface+=2))
+        ((iface+=1))
+    done
+
+    #iface=1
+
+    #num_inc=$(($1+1))
+    #while [ $iface -lt $num_inc ]
+    while [ $iface -lt $1 ]
+    do
+        args_1="$args_1 -s $RUN_DIR/eth.$iface"
+        #((iface+=2))
+        ((iface+=1))
+    done
+    $SWITCH_EXE -m 0 -S 500 -E 500 \
+    $args_0 -h $RUN_DIR/s0eth > $RUN_DIR/log.switch &
+
+    pid=$!
+    ALL_PIDS="$ALL_PIDS $pid"
+    SWITCH_PIDS="$SWITCH_PIDS $pid"
+    sleep 1
+
+    $SWITCH_EXE -m 0 -S 500 -E 500 \
+    $args_1 -s $RUN_DIR/s0eth > $RUN_DIR/log.switch &
+    pid=$!
+    ALL_PIDS="$ALL_PIDS $pid"
+    SWITCH_PIDS="$SWITCH_PIDS $pid"
+
     return $pid
 }
 
@@ -63,13 +106,18 @@ do
 done
 
 sleep 2
-run_switch $1
-SWITCH_PID=$!
+#run_switch $1
+run_switch_dumbbell $1
+#SWITCH_PID=$!
 
 for p in $PKTGEN_PIDS ; do
     wait $p
 done
 
-echo "kill switch"
-kill -9 $SWITCH_PID
+echo "Pktgen Done, kill switch"
+#kill -9 $SWITCH_PID
+
+for p in $SWITCH_PIDS ; do
+    kill -9 $p
+done
 date
