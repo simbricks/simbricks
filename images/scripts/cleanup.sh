02040d8c7381a5a03b1a5fed3d8adf6f7ce0b419
Antoine Kaufmann <antoinek@mpi-sws.org>
2022-02-19 10:09:55 +0100

    images: simplify cleanup script

--- images/scripts/cleanup.sh
+++ images/scripts/cleanup.sh
@@ -1,24 +1,8 @@
 #!/bin/bash -eux
 
-# Make sure udev does not block our network - http://6.ptmc.org/?p=164
-echo "==> Cleaning up udev rules"
-rm -rf /dev/.udev/
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
+# Cleaning up leftover dhcp leases
+rm -f /var/lib/dhcp3/* /var/lib/dhcp/*
 
-echo "==> Cleaning up leftover dhcp leases"
-# Ubuntu 10.04
-if [ -d "/var/lib/dhcp3" ]; then
-    rm /var/lib/dhcp3/*
-fi
-# Ubuntu 12.04 & 14.04
-if [ -d "/var/lib/dhcp" ]; then
-    rm /var/lib/dhcp/*
-fi 
-
-# Add delay to prevent "vagrant reload" from failing
-echo "pre-up sleep 2" >> /etc/network/interfaces
-
-echo "==> Cleaning up tmp"
 rm -rf /tmp/*
 
 # Cleanup apt cache
@@ -26,12 +10,6 @@ apt-get -y autoremove --purge
 apt-get -y clean
 apt-get -y autoclean
 
-echo "==> Installed packages"
-dpkg --get-selections | grep -v deinstall
-
-DISK_USAGE_BEFORE_CLEANUP=$(df -h)
-
-# Remove Bash history
 unset HISTFILE
 rm -f /root/.bash_history
 rm -f /home/ubuntu/.bash_history
@@ -39,7 +17,7 @@ rm -f /home/ubuntu/.bash_history
 # Clean up log files
 find /var/log -type f | while read f; do echo -ne '' > "${f}"; done;
 
-echo "==> Clearing last login information"
+# Clearing last login information
 >/var/log/lastlog
 >/var/log/wtmp
 >/var/log/btmp
