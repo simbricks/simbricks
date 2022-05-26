06ca4ac8c7b292d0d164ab211b5a6b751bf61c6b
Antoine Kaufmann <antoinek@mpi-sws.org>
2022-05-26 14:56:37 +0200

    docker: add missing cleanup_gem5 script

---
+++ docker/cleanup_gem5.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+mkdir -p sims/external/gem5-new/
+cd sims/external/gem5
+cp -r --parent \
+  build/X86/gem5.fast \
+  configs \
+  ../gem5-new/
+#`find build -name \*.py` \
+cd ..
+git submodule deinit -f gem5
+rm -rf ../../.git/modules/sims/external/gem5
+rm -rf gem5
+mv gem5-new gem5
+touch gem5/ready
