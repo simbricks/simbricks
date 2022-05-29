0fb7d1e74933af8ab5564657e92b795379955cd5
Hejing Li <hajeongee@gmail.com>
2022-05-29 16:15:18 +0200

    collect scripts for artifact evaluation under ae subdir

---
+++ experiments/pyexps/ae/ns3-dctcp.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+# Copyright 2021 Max Planck Institute for Software Systems, and
+# National University of Singapore
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+###########################################################################
+# This script runs dctcp experiment in standalone ns-3
+# 
+##### build dctcp-modes.cc example in ns-3
+##### cp examples/tcp/dctcp-modes.cc scratch/dctcp-modes.cc
+##### ./waf
+
+##### ./ns3-dctcp.sh [num_core] 
+
+
+EHSIM_BASE="$(readlink -f $(dirname ${BASH_SOURCE[0]})/../..)"
+NS3_BASE="$EHSIM_BASE/sims/external/ns-3"
+OUTDIR_BASE="$EHSIM_BASE/experiments/pyexps"
+
+cd $NS3_BASE
+k_start=0
+k_end=199680
+#k_step=2080
+#k_end=199680
+k_step=8320
+#mtus="1500 4000 9000"
+mtus="4000"
+# link latency corresponds to RTT latency 1us 10us 100us 200us
+#latencies="167ns 1670ns 16us 33us"
+latencies="16us"
+cores=$1
+
+echo $cores
+
+proc=0
+pids=""
+
+cleanup() {
+    echo Cleaning up
+    for p in $pids ; do
+        kill $p &>/dev/null
+    done
+
+    sleep 1
+    for p in $pids ; do
+        kill -KILL $p &>/dev/null
+    done
+
+}
+
+sighandler() {
+    echo "Caught Interrupt, aborting...."
+    cleanup
+    exit 1
+}
+
+
+trap "sighandler" SIGINT
+
+#for k in $(seq $k_start $k_step $k_end)
+for lat in $latencies
+do
+    for m in $mtus
+    do
+        #echo $k
+        #for m in 1500 4000 9000 # MTU size
+        for k in $(seq $k_start $k_step $k_end)
+        do
+            echo "latency: $lat MtU: $m  K: $k  "
+            ./cosim-dctcp-run.sh $k $m $lat &
+            pid=$!
+            pids="$pids $pid"
+            proc=$(($proc + 1))
+            
+            if [ $proc -eq $cores ]; then
+                for p in $pids; do
+                    wait $p
+                done
+                proc=0
+                pids=""
+
+            fi
+        done
+    done
+done
+
+for p in $pids; do
+    wait $p
+done
+
---
+++ experiments/pyexps/ae/t1_combination.py
@@ -0,0 +1,109 @@
+# Copyright 2021 Max Planck Institute for Software Systems, and
+# National University of Singapore
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+########################################################################
+# This script is for reproducing [Table 1] in the paper.
+# It generates experiments for all combinations of simulation.
+#
+# Host type has qemu-kvm(qemu in short), gem5-timing-mode(gt), qemu-timing-mode(qt)
+# Nic type has Intel_i40e behavioral model(ib), corundum behavioral model(cb), corundum verilator(cv)
+# Net type has Switch behavioral model(sw), ns-3(ns3)
+#
+# In each simulation, two hosts are connected by a switch 
+# [HOST_0] - [NIC_0] ---- [SWITCH] ----  [NIC_1] - [HOST_1]
+#  server                                           client
+# 
+# The server host runs netperf server and client host runs TCP_RR and
+# TCP_STREAM test
+# 
+# The command to run all the experiments is:
+# $: python3 run.py pyexps/ae/t1_combination.py --filter nf-* --verbose
+########################################################################
+
+
+import simbricks.experiments as exp
+import simbricks.simulators as sim
+import simbricks.nodeconfig as node
+from simbricks.simulator_utils import create_basic_hosts
+
+host_types = ['qemu', 'gt', 'qt']
+nic_types = ['ib', 'cb', 'cv']
+net_types = ['sw', 'ns3']
+experiments = []
+
+# Create multiple experiments with different simulator permutations, which can
+# be filtered later.
+for host_type in host_types:
+    for nic_type in nic_types:
+        for net_type in net_types:
+            e = exp.Experiment('nf-' + host_type + '-' + net_type + '-' + nic_type)
+
+            # network
+            if net_type == 'sw':
+                net = sim.SwitchNet()
+            elif net_type == 'ns3':
+                net = sim.NS3BridgeNet()
+            else:
+                raise NameError(net_type)
+            e.add_network(net)
+
+            # host
+            if host_type == 'qemu':
+                host_class = sim.QemuHost
+            elif host_type == 'qt':
+                def qemu_timing():
+                    h = sim.QemuHost()
+                    h.sync = True
+                    return h
+                host_class = qemu_timing
+            elif host_type == 'gt':
+                host_class = sim.Gem5Host
+                e.checkpoint = True
+            else:
+                raise NameError(host_type)
+
+            # nic
+            if nic_type == 'ib':
+                nic_class = sim.I40eNIC
+                nc_class = node.I40eLinuxNode
+            elif nic_type == 'cb':
+                nic_class = sim.CorundumBMNIC
+                nc_class = node.CorundumLinuxNode
+            elif nic_type == 'cv':
+                nic_class = sim.CorundumVerilatorNIC
+                nc_class = node.CorundumLinuxNode
+            else:
+                raise NameError(nic_type)
+
+            # create servers and clients
+            servers = create_basic_hosts(e, 1, 'server', net, nic_class, host_class,
+                    nc_class, node.NetperfServer)
+
+            clients = create_basic_hosts(e, 1, 'client', net, nic_class, host_class,
+                    nc_class, node.NetperfClient, ip_start = 2)
+
+            for c in clients:
+                c.wait = True
+                c.node_config.app.server_ip = servers[0].node_config.ip
+
+            # add to experiments
+            experiments.append(e)
