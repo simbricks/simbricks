a2f036196cccc2d5c47e448cff86daf4b405250e
Hejing Li <hajeongee@gmail.com>
2022-06-05 15:24:29 +0200

    ae: add dist-gem5 experiment script

---
+++ experiments/ae/dist-gem5.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SB_BASE="$(readlink -f $(dirname ${BASH_SOURCE[0]})/../..)"
+GEM5_BASE="$SB_BASE/sims/external/gem5"
+
+# Runs dist-gem5 data points in Figure 9
+# It will generate simulation result files in simbricks/sims/external/gem5/util/dist/test/run-*.out
+echo "Start running dist-gem5 data points"
+cd $GEM5_BASE/util/dist/test
+./exp_run.sh
+echo "Done running dist-gem5 data points"
+
+# Runs Simbricks data points in Figure 9
+# It will generate simulation results in simbricks/sims/external/gem5/util/simbricks/run-*.out
+echo "Start running simbricks data points"
+cd $GEM5_BASE/util/simbricks
+./exp_run.sh
+echo "Done running simbricks data points"
+
+# Process the results and prints
+python3 pyexps/ae/data_dist.py $NS3_BASE > ae/dist.data
+
