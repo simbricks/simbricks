d6d1d6d0a3fcfe5632709ef43d642f6e5d03bc3e
Hejing Li <hajeongee@gmail.com>
2022-06-09 10:17:20 +0200

    ae: host cale typo

--- experiments/ae/host-scale.sh
+++ experiments/ae/host-scale.sh
@@ -2,7 +2,7 @@
 
 # Runs simulation with varying PCI latency configuration
 # It will generate raw simulation json file result in out/
-python3 run.py pyexps/ae/f7_sacle.py --filter host-* --force --verbose
+python3 run.py pyexps/ae/f7_scale.py --filter host-* --force --verbose
 
 # Process the results and prints
 python3 pyexps/ae/data_host_scale.py out/ > ae/host_scale.data
