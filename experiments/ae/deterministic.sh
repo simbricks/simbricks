5824279194cabd9dbb8320765fe3bf4dbf1ed3cf
Antoine Kaufmann <antoinek@mpi-sws.org>
2022-06-12 12:33:15 +0200

    experiments: add separate experiments/ae/deterministic-parse.sh script

--- experiments/ae/deterministic.sh
+++ experiments/ae/deterministic.sh
@@ -10,16 +10,5 @@ else
     run_num=$1
 fi
 python3 run.py pyexps/ae/determ.py --filter dt-gt-ib-sw --force --verbose --runs=$1
-START=1
 
-# Parse the json file into experiment's out_dir
-for i in $(seq 1 $run_num);
-do
-    mkdir -p out/dt-gt-ib-sw/${i}
-    python3 pyexps/log_parser.py out/dt-gt-ib-sw-$i.json
-    
-    cat out/dt-gt-ib-sw/${i}/host.client.0 | awk '/system.pc.simbricks_0:/ {print $1}' > out/dt-gt-ib-sw/${i}/host_trim
-done
-
-
-diff out/dt-gt-ib-sw/1/host_trim out/dt-gt-ib-sw/2/host_trim > out/dt-gt-ib-sw/host_trim12.diff
+./ae/deterministic-parse.sh $run_num out
