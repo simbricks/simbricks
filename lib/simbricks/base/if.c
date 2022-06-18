2ae1ca5da504be1b85ee57dcc7146465eec99c24
Antoine Kaufmann <antoinek@mpi-sws.org>
2022-06-18 14:03:24 +0200

    lib/simbricks/base: Send termination message in SimbricksBaseIfClose()

--- lib/simbricks/base/if.c
+++ lib/simbricks/base/if.c
@@ -809,8 +809,10 @@ void SimbricksBaseIfClose(struct SimbricksBaseIf *base_if)
   }
 
   if (base_if->conn_state == kConnOpen) {
-    /* send out sync with max time stamp so no one else is stuck */
-    while (SimbricksBaseIfOutSync(base_if, UINT64_MAX) != 0);
+    // send out termination message
+    volatile union SimbricksProtoBaseMsg *msg;
+    while ((msg = SimbricksBaseIfOutAlloc(base_if, UINT64_MAX)) == NULL);
+    SimbricksBaseIfOutSend(base_if, msg, SIMBRICKS_PROTO_MSG_TYPE_TERMINATE);
   }
 
   close(base_if->conn_fd);
