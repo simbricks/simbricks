d7b82a92ef9a7b76c2da434cd576b206eb15f75c
Jonas Kaufmann <jokau03@gmail.com>
2022-07-02 17:57:37 +0200

    fix pytype errors in results/

--- results/nopaxos.py
+++ results/nopaxos.py
@@ -20,7 +20,6 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import itertools
 import sys
 
 import utils.parse_nopaxos
