986dd0228ba0fde099a01f506a3acc78f76e14d0
Antoine Kaufmann <antoinek@mpi-sws.org>
2022-09-22 16:05:40 +0200

    images: update to kernel 5.15.93
    This is the newest LTS kernel release. Will also come in handy for
    disaggregated memory stuff later.

--- images/scripts/install-base.sh
+++ images/scripts/install-base.sh
@@ -4,8 +4,8 @@ set -eux
 
 pushd /tmp/input
 mv guestinit.sh /home/ubuntu/guestinit.sh
-mv bzImage /boot/vmlinuz-5.4.46
-mv config-5.4.46 /boot/
+mv bzImage /boot/vmlinuz-5.15.93
+mv config-5.15.93 /boot/
 mv m5 /sbin/m5
 update-grub
 tar xf kheaders.tar.bz2 -C /
