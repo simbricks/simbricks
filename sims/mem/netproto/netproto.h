906a282cbeb5daa4e3caef41f709f899fd1cf47a
Hejing Li <hajeongee@gmail.com>
2022-09-27 23:45:08 +0200

    sims/mem/netproto: add network memory protocol definition
    A very simple UDP-based protocol for accessing network-attached memories.

---
+++ sims/mem/netproto/netproto.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022 Max Planck Institute for Software Systems, and
+ * National University of Singapore
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef SIMS_MEM_NETPROTO_NETPROTO_H_
+#define SIMS_MEM_NETPROTO_NETPROTO_H_
+
+#include <stdint.h>
+
+struct MemOp {
+  uint8_t OpType;
+  uint64_t req_id;
+  uint64_t addr;
+  uint16_t len;  // length of data if there is any
+  uint64_t as_id;
+  uint8_t pad[37];
+  uint8_t data[];
+} __attribute__((packed));
+
+#endif  // SIMS_MEM_NETPROTO_NETPROTO_H_
