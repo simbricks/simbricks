9628615d8369cbb47495dc6cacc06e00ef612c97
Jonas Kaufmann <jokau03@gmail.com>
2023-01-13 18:15:55 +0100

    images/base: set init kernel parameter to run experiment code by default

--- images/scripts/install-base.sh
+++ images/scripts/install-base.sh
@@ -21,6 +21,9 @@ mv guestinit.sh /home/ubuntu/guestinit.sh
 mv bzImage /boot/vmlinuz-5.15.93
 mv config-5.15.93 /boot/
 mv m5 /sbin/m5
+echo "GRUB_CMDLINE_LINUX_DEFAULT=\"console=tty1 console=ttyS0 " \
+     "init=/home/ubuntu/guestinit.sh rw\"" \
+    >> /etc/default/grub.d/50-cloudimg-settings.cfg
 update-grub
 
 # with stupid ubuntu22 /lib is a symlink at which point just untaring to / will
