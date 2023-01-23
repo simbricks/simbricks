706e67bcae10eeebf4f1b327aa9d605d338b42a7
Jonas Kaufmann <jokau03@gmail.com>
2023-01-23 12:34:33 +0100

    images/base: skip grub menu

--- images/scripts/install-base.sh
+++ images/scripts/install-base.sh
@@ -21,9 +21,13 @@ mv guestinit.sh /home/ubuntu/guestinit.sh
 mv bzImage /boot/vmlinuz-5.15.93
 mv config-5.15.93 /boot/
 mv m5 /sbin/m5
+
+GRUB_CFG_FILE=/etc/default/grub.d/50-cloudimg-settings.cfg
+echo 'GRUB_DISABLE_OS_PROBER=true' >> $GRUB_CFG_FILE
+echo 'GRUB_HIDDEN_TIMEOUT=0' >> $GRUB_CFG_FILE
+echo 'GRUB_TIMEOUT=0' >> $GRUB_CFG_FILE
 echo "GRUB_CMDLINE_LINUX_DEFAULT=\"console=tty1 console=ttyS0 " \
-     "init=/home/ubuntu/guestinit.sh rw\"" \
-    >> /etc/default/grub.d/50-cloudimg-settings.cfg
+     "init=/home/ubuntu/guestinit.sh rw\"" >> $GRUB_CFG_FILE
 update-grub
 
 # with stupid ubuntu22 /lib is a symlink at which point just untaring to / will
