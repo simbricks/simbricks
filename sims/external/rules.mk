2758151d576d962e63acf88d96c3625f03552a1d
Marvin Meiers <mmeiers@mpi-sws.org>
2023-09-04 20:11:27 +0200

    sims/external: add clean target for external simulators
    Add new target `clean-external` to the Makefile, which triggers the
    clean task of the respective buid system for each external simulator.
    Each simulator can also be cleaned individually by using the targets
    `{simulator name}-clean` (e.g. `gem5-clean`).

--- sims/external/rules.mk
+++ sims/external/rules.mk
@@ -26,10 +26,12 @@ QEMU_IMG := $(d)qemu/build/qemu-img
 QEMU := $(d)qemu/build/qemu-system-x86_64
 GEM5_VARIANT ?= fast
 
+EXTERNAL_SIMS_DIR := $(d)
+
 $(eval $(call subdir,simics))
 
 external: $(d)gem5/ready $(d)qemu/ready $(d)ns-3/ready $(d)femu/ready
-.PHONY: external
+.PHONY: external gem5-clean qemu-clean ns-3-clean femu-clean
 
 $(d)gem5:
 	git clone https://github.com/simbricks/gem5.git $@
@@ -40,6 +42,10 @@ $(d)gem5/ready: $(d)gem5
 		LIBPATH="$(abspath $(lib_dir))" \
 	    -j`nproc`
 	touch $@
+
+gem5-clean:
+	-cd $(EXTERNAL_SIMS_DIR)gem5 && scons --clean build/X86/gem5.$(GEM5_VARIANT)
+	rm -f $(EXTERNAL_SIMS_DIR)gem5/ready
 
 
 $(d)qemu:
@@ -63,6 +69,10 @@ $(QEMUG_IMG): $(d)qemu/ready
 $(QEMU): $(d)qemu/ready
 	touch $@
 
+qemu-clean:
+	-+cd $(EXTERNAL_SIMS_DIR)qemu && $(MAKE) clean
+	rm -f $(EXTERNAL_SIMS_DIR)qemu/ready
+
 
 $(d)ns-3:
 	git clone https://github.com/simbricks/ns-3.git $@
@@ -70,6 +80,10 @@ $(d)ns-3:
 $(d)ns-3/ready: $(d)ns-3 $(lib_netif)
 	+cd $< && COSIM_PATH=$(abspath $(base_dir)) ./cosim-build.sh configure
 	touch $@
+
+ns-3-clean:
+	-cd $(EXTERNAL_SIMS_DIR)ns-3 && ./waf clean
+	rm -f $(EXTERNAL_SIMS_DIR)ns-3/ready
 
 $(d)femu:
 	git clone https://github.com/simbricks/femu.git $@
@@ -81,5 +95,10 @@ $(d)femu/ready: $(d)femu $(lib_nicif)
 	    EXTRA_CPPFLAGS=-I$(abspath $(lib_dir))
 	touch $@
 
+femu-clean:
+	-+cd $(EXTERNAL_SIMS_DIR)femu && $(MAKE) clean
+	rm -f $(EXTERNAL_SIMS_DIR)femu/ready
+
 DISTCLEAN := $(d)gem5 $(d)qemu $(d)ns-3 $(d)femu
+EXTERNAL_CLEAN_TASKS := gem5-clean qemu-clean ns-3-clean femu-clean
 include mk/subdir_post.mk
