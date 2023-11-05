6f317baf793f304b1db4d5f437bc21c2cb9b63d7
Jonas Kaufmann <jokau03@gmail.com>
2023-11-05 11:12:02 +0100

    images: hot-fix build process for images derived from base failing
    hot-fix for #75 but temporarliy breaks experiments that use Simics. We will fix
    this in a later commit.

--- images/scripts/install-base.sh
+++ images/scripts/install-base.sh
@@ -26,8 +26,6 @@ GRUB_CFG_FILE=/etc/default/grub.d/50-cloudimg-settings.cfg
 echo 'GRUB_DISABLE_OS_PROBER=true' >> $GRUB_CFG_FILE
 echo 'GRUB_HIDDEN_TIMEOUT=0' >> $GRUB_CFG_FILE
 echo 'GRUB_TIMEOUT=0' >> $GRUB_CFG_FILE
-echo "GRUB_CMDLINE_LINUX_DEFAULT=\"console=tty1 console=ttyS0 " \
-     "init=/home/ubuntu/guestinit.sh rw\"" >> $GRUB_CFG_FILE
 update-grub
 
 # with stupid ubuntu22 /lib is a symlink at which point just untaring to / will
