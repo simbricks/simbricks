1e41de5adf06f6acafd5f165718928c2927fe357
Hejing Li <hajeongee@gmail.com>
2024-06-20 11:56:31 +0000

    simple_ping_sysconf.py: use LinuxHost and set driver as a list

--- experiments/pyexps/simple_ping_sysconf.py
+++ experiments/pyexps/simple_ping_sysconf.py
@@ -12,16 +12,16 @@ HOST0 -- NIC0 ------ SWITCH ------ NIC1 -- HOST1
 system = spec.System()
 
 # create a host instance and a NIC instance then install the NIC on the host
-host0 = spec.Host(system)
+host0 = spec.LinuxHost(system)
 nic0 = spec.i40eNIC(system)
-host0.nic_driver = 'i40e'
+host0.nic_driver = ['i40e']
 host0.ip = '10.0.0.1'
 pcichannel0 = spec.PCI(system)
 pcichannel0.install(host0, nic0)
 
-host1 = spec.Host(system)
+host1 = spec.LinuxHost(system)
 nic1 = spec.i40eNIC(system)
-host1.nic_driver = 'i40e'
+host1.nic_driver = ['i40e']
 host1.ip = '10.0.0.2'
 pcichannel1 = spec.PCI(system)
 pcichannel1.install(host1, nic1)
