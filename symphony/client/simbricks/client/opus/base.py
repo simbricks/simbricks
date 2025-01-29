3be375e450b69f2b6b8b8342e2f5ae2b89e143b6
Jakob Görgen <jakob@simbricks.io>
2025-01-29 13:45:08 +0100

    symphony/client/opus: fixed _fetch_next_output datetime error

--- symphony/client/simbricks/client/opus/base.py
+++ symphony/client/simbricks/client/opus/base.py
@@ -60,9 +60,7 @@ class ConsoleLineGenerator:
             for _, output_lines in simulator.commands.items():
                 for output_line in output_lines:
                     lines.append((simulator.name, output_line.output))
-                    self._simulators_seen_until[simulator_id] = datetime.datetime.fromisoformat(
-                        output_line.created_at
-                    )
+                    self._simulators_seen_until[simulator_id] = output_line.created_at
 
         return lines
 
