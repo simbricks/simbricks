fa8009b5207c94e333d74817f8a018dbb18f283e
Marvin Meiers <mmeiers@mpi-sws.org>
2025-04-10 08:57:27 +0200

    symphony/runner: use loop to read all data when receiving events

--- symphony/runner/simbricks/runner/utils.py
+++ symphony/runner/simbricks/runner/utils.py
@@ -11,12 +11,22 @@ async def send_events(
     data = bytes(f"{len(payload):12x}", encoding="utf-8") + payload
     await write(data)
 
+async def _read_all(read: abc.Callable[[int], abc.Awaitable[bytes]], length: int) -> bytes:
+    assert length >= 0
+    data = bytes()
+    while len(data) < length:
+        d = await read(length - len(data))
+        if (length - len(data)) != 0 and len(d) == 0:
+            raise RuntimeError("connection broken")
+        data += d
+    return data
+
 async def get_events(
         read: abc.Callable[[int], abc.Awaitable[bytes]]
 ) -> tuple[schemas.ApiEventType, schemas.ApiEventBundle]:
-    length_str = (await read(12)).decode("utf-8")
+    length_str = (await _read_all(read, 12)).decode("utf-8")
     length = int(length_str, 16)
-    payload = (await read(length)).decode("utf-8")
+    payload = (await _read_all(read, length)).decode("utf-8")
 
     separator = payload.find(",")
     if separator == -1:
