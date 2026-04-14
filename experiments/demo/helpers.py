1b0a91c03a45d2837dcd5bd8597f883598fa921d
Jakob Görgen <jakob@simbricks.io>
2026-04-14 12:14:50 +0200

    experiments: add demo subfolder

---
+++ experiments/demo/helpers.py
@@ -0,0 +1,28 @@
+from simbricks.orchestration import system
+import re
+
+
+# Custom defined helper function to create an 'I40ELinuxHost' attached to an 'IntelI40eNIC'.
+def sys_host_nic(sys, image, ip, hn=None, nn=None):
+    host = system.I40ELinuxHost(sys)
+    host.add_disk(image)
+    host.add_disk(system.LinuxConfigDiskImage(sys, host))
+    if hn:
+        host.name = hn
+
+    nic = system.IntelI40eNIC(sys)
+    nic.add_ipv4(ip)
+    host.connect_pcie_dev(nic)
+    if nn:
+        nic.name = nn
+
+    return host, nic
+
+
+def parse_Iperf_line_bytes(line: str) -> float | None:
+    pattern = r"(\d+\.?\d*)\s*MBytes"
+    match = re.search(pattern, line)
+    if not match:
+        return None
+    m_bytes = float(match.group(1))
+    return m_bytes
---
+++ experiments/demo/loop.py
@@ -0,0 +1,69 @@
+import sys, os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+if current_dir not in sys.path:
+    sys.path.insert(0, current_dir)
+
+from simbricks.orchestration import simulation as sim
+from simbricks.orchestration import system
+from simbricks.orchestration.helpers import instantiation as inst_helpers
+from simbricks.orchestration.helpers import simulation as sim_helpers
+from helpers import sys_host_nic
+
+instantiations = []
+
+iperf_rates = ["150m", "430m", "600m"]
+for rate in iperf_rates:
+
+    #################################
+    # System configuration
+    #################################
+
+    sys = system.System()
+
+    # We create a Linux disk image instance that will be used by the hosts we create.
+    # The image used here is provided by SimBricks, user can however also provide custom images.
+    distro_disk_image = system.DistroDiskImage(sys, "base")
+
+    # Configure the server to start an Iperf server by adding an application to the server object.
+    server_host, server_nic = sys_host_nic(
+        sys, distro_disk_image, "10.0.0.1", "Iperf-Server", "Server-NIC"
+    )
+    server_host.add_app(system.IperfUDPServer(server_host))
+
+    # Configure the client to start an Iperf client by adding an application to the client object.
+    # Besides, we set the wait flag on the application to tell SimBricks to run until this application is completed.
+    client_host, client_nic = sys_host_nic(
+        sys, distro_disk_image, "10.0.0.2", "Iperf-Client", "Client-NIC"
+    )
+    ping_client_app = system.IperfUDPClient(client_host, server_nic._ip, rate)
+    ping_client_app.wait = True
+    client_host.add_app(ping_client_app)
+
+    # Create a network switch that connects the server and client NICs with each other.
+    switch0 = system.EthSwitch(sys)
+    for nic in [server_nic, client_nic]:
+        switch0.connect_eth_peer_if(nic._eth_if)
+
+    #################################
+    # Simulation configuration
+    #################################
+
+    # We make a simulator choice by simply mapping component types to simulators.
+    simulation = sim_helpers.simple_simulation(
+        sys,
+        compmap={
+            system.FullSystemHost: sim.QemuSim,
+            system.IntelI40eNIC: sim.I40eNicSim,
+            system.EthSwitch: sim.SwitchNet,
+        },
+    )
+    # simulation.enable_synchronization()
+
+    #################################
+    # Instantiation configuration
+    #################################
+
+    # Instantiate the virtual prototype
+    instantiation = inst_helpers.simple_instantiation(simulation)
+    instantiations.append(instantiation)
