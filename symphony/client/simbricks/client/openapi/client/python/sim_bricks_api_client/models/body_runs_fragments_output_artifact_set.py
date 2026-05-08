835e2b91fb9c11728621d97f57bb0786e646fe9b
Jakob Görgen <jakob@simbricks.io>
2026-05-08 10:48:43 +0200

    symphony/simbricks/client/openapi: update client - name attr sys, sim, inst and run

--- symphony/client/simbricks/client/openapi/client/python/sim_bricks_api_client/models/body_runs_fragments_output_artifact_set.py
+++ symphony/client/simbricks/client/openapi/client/python/sim_bricks_api_client/models/body_runs_fragments_output_artifact_set.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
 from collections.abc import Mapping
-from io import BytesIO
 from typing import Any, TypeVar, cast
 
 from attrs import define as _attrs_define
 from attrs import field as _attrs_field
 
-from ..types import UNSET, File, FileTypes, Unset
+from ..types import UNSET, Unset
 
 T = TypeVar("T", bound="BodyRunsFragmentsOutputArtifactSet")
 
@@ -16,28 +15,21 @@ T = TypeVar("T", bound="BodyRunsFragmentsOutputArtifactSet")
 class BodyRunsFragmentsOutputArtifactSet:
     """
     Attributes:
-        file (File | list[File | str] | str | Unset):
+        file (list[str] | str | Unset):
     """
 
-    file: File | list[File | str] | str | Unset = UNSET
+    file: list[str] | str | Unset = UNSET
     additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> dict[str, Any]:
-        file: FileTypes | list[FileTypes | str] | str | Unset
+        file: list[str] | str | Unset
         if isinstance(self.file, Unset):
             file = UNSET
-        elif isinstance(self.file, File):
-            file = self.file.to_tuple()
-
         elif isinstance(self.file, list):
             file = []
             for file_type_2_item_data in self.file:
-                file_type_2_item: FileTypes | str
-                if isinstance(file_type_2_item_data, File):
-                    file_type_2_item = file_type_2_item_data.to_tuple()
-
-                else:
-                    file_type_2_item = file_type_2_item_data
+                file_type_2_item: str
+                file_type_2_item = file_type_2_item_data
                 file.append(file_type_2_item)
 
         else:
@@ -55,17 +47,9 @@ class BodyRunsFragmentsOutputArtifactSet:
     def from_dict(cls: type[T], src_dict: Mapping[str, Any]) -> T:
         d = dict(src_dict)
 
-        def _parse_file(data: object) -> File | list[File | str] | str | Unset:
+        def _parse_file(data: object) -> list[str] | str | Unset:
             if isinstance(data, Unset):
                 return data
-            try:
-                if not isinstance(data, bytes):
-                    raise TypeError()
-                file_type_0 = File(payload=BytesIO(data))
-
-                return file_type_0
-            except (TypeError, ValueError, AttributeError, KeyError):
-                pass
             try:
                 if not isinstance(data, list):
                     raise TypeError()
@@ -73,16 +57,8 @@ class BodyRunsFragmentsOutputArtifactSet:
                 _file_type_2 = data
                 for file_type_2_item_data in _file_type_2:
 
-                    def _parse_file_type_2_item(data: object) -> File | str:
-                        try:
-                            if not isinstance(data, bytes):
-                                raise TypeError()
-                            file_type_2_item_type_1 = File(payload=BytesIO(data))
-
-                            return file_type_2_item_type_1
-                        except (TypeError, ValueError, AttributeError, KeyError):
-                            pass
-                        return cast(File | str, data)
+                    def _parse_file_type_2_item(data: object) -> str:
+                        return cast(str, data)
 
                     file_type_2_item = _parse_file_type_2_item(file_type_2_item_data)
 
@@ -91,7 +67,7 @@ class BodyRunsFragmentsOutputArtifactSet:
                 return file_type_2
             except (TypeError, ValueError, AttributeError, KeyError):
                 pass
-            return cast(File | list[File | str] | str | Unset, data)
+            return cast(list[str] | str | Unset, data)
 
         file = _parse_file(d.pop("file", UNSET))
 
--- symphony/client/simbricks/client/openapi/client/python/sim_bricks_api_client/models/run.py
+++ symphony/client/simbricks/client/openapi/client/python/sim_bricks_api_client/models/run.py
@@ -18,6 +18,7 @@ class Run:
 
     Attributes:
         id (None | str | Unset): API Object id
+        name (None | str | Unset):
         namespace_id (None | str | Unset): API Object id
         instantiation_id (None | str | Unset): API Object id
         output (None | str | Unset):
@@ -25,6 +26,7 @@ class Run:
     """
 
     id: None | str | Unset = UNSET
+    name: None | str | Unset = UNSET
     namespace_id: None | str | Unset = UNSET
     instantiation_id: None | str | Unset = UNSET
     output: None | str | Unset = UNSET
@@ -37,6 +39,12 @@ class Run:
             id = UNSET
         else:
             id = self.id
+
+        name: None | str | Unset
+        if isinstance(self.name, Unset):
+            name = UNSET
+        else:
+            name = self.name
 
         namespace_id: None | str | Unset
         if isinstance(self.namespace_id, Unset):
@@ -69,6 +77,8 @@ class Run:
         field_dict.update({})
         if id is not UNSET:
             field_dict["id"] = id
+        if name is not UNSET:
+            field_dict["name"] = name
         if namespace_id is not UNSET:
             field_dict["namespace_id"] = namespace_id
         if instantiation_id is not UNSET:
@@ -92,6 +102,15 @@ class Run:
             return cast(None | str | Unset, data)
 
         id = _parse_id(d.pop("id", UNSET))
+
+        def _parse_name(data: object) -> None | str | Unset:
+            if data is None:
+                return data
+            if isinstance(data, Unset):
+                return data
+            return cast(None | str | Unset, data)
+
+        name = _parse_name(d.pop("name", UNSET))
 
         def _parse_namespace_id(data: object) -> None | str | Unset:
             if data is None:
@@ -139,6 +158,7 @@ class Run:
 
         run = cls(
             id=id,
+            name=name,
             namespace_id=namespace_id,
             instantiation_id=instantiation_id,
             output=output,
