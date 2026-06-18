dd0d298394cf338864963b34295d0b90fee05089
Jakob Görgen <jakob@simbricks.io>
2026-06-18 15:53:14 +0200

    docker/run_local_executor: add proxy_host_ip parameter, see 530591302e0fe59f4c2090ab05445ebf3864b9ae

--- docker/run_local_executor.sh
+++ docker/run_local_executor.sh
@@ -2,6 +2,7 @@
 
 ip=$1
 port=$2
+proxy_host_ip=$3
 
 if [[ "$ip" = "" ]] || [[ "$port" = "" ]]; then
     echo "Error: you need to specify both ip and port"
@@ -11,4 +12,4 @@ fi
 
 sudo chmod o+rw /dev/kvm
 make convert-images-raw
-exec simbricks-executor-local "$ip" "$port"
+exec simbricks-executor-local "$ip" "$port" "$proxy_host_ip"
