e688c0809bda2d75dcf4e47f64b1df05af9bca79
Marvin Meiers <marvin@simbricks.io>
2026-07-02 15:19:28 +0200

    symphony: move mem simulators to separate component package

--- symphony/components/mem_base_sim/simbricks/components/mem/simulation/base/mem.py
+++ symphony/components/mem_base_sim/simbricks/components/mem/simulation/base/mem.py
@@ -1,5 +1,5 @@
-# Copyright 2025 Max Planck Institute for Software Systems, and
-# National University of Singapore
+# Copyright 2026 Max Planck Institute for Software Systems,
+# National University of Singapore, and SimBricks UG (haftungsbeschränkt)
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
