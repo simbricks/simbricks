2465a61a35ec16b67657ac6732a2b49af6e79035
Marvin Meiers <marvin@simbricks.io>
2026-07-22 18:13:06 +0200

    conda-recipes: add conda recipes for SimBricks Python packages

--- conda-recipes/rules.mk
+++ conda-recipes/rules.mk
@@ -3,12 +3,38 @@ OUTPUT_FLAG := $(if $(OUTPUT_FOLDER),--output-folder $(OUTPUT_FOLDER))
 
 BASE_BUILD_CMD := conda build -m conda-recipes/conda_build_config.yaml $(OUTPUT_FLAG)
 
-.PHONY: conda-packages lib dist
+.PHONY: conda-packages lib dist cli-py client-py local-py orchestration-py \
+	runner-py runtime-py telemetry-py utils-py
 
-conda-packages: lib dist
+conda-packages: lib dist cli-py client-py local-py orchestration-py runner-py \
+	runtime-py telemetry-py utils-py
 
 lib:
 	$(BASE_BUILD_CMD) conda-recipes/simbricks-lib
 
 dist: 
 	$(BASE_BUILD_CMD) conda-recipes/simbricks-dist
+
+cli-py: client-py orchestration-py telemetry-py
+	$(BASE_BUILD_CMD) conda-recipes/simbricks-cli
+
+client-py: orchestration-py
+	$(BASE_BUILD_CMD) conda-recipes/simbricks-client
+
+local-py: orchestration-py runtime-py
+	$(BASE_BUILD_CMD) conda-recipes/simbricks-local
+
+orchestration-py: utils-py
+	$(BASE_BUILD_CMD) conda-recipes/simbricks-orchestration
+
+runner-py: orchestration-py runtime-py client-py telemetry-py
+	$(BASE_BUILD_CMD) conda-recipes/simbricks-runner
+
+runtime-py: orchestration-py utils-py
+	$(BASE_BUILD_CMD) conda-recipes/simbricks-runtime
+
+telemetry-py: client-py
+	$(BASE_BUILD_CMD) conda-recipes/simbricks-telemetry
+
+utils-py:
+	$(BASE_BUILD_CMD) conda-recipes/simbricks-utils
